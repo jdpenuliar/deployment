325c86e317350f52e74521c3767b531167b84cc3
jdpenuliar <jaydz.penuliar@gmail.com>
2016-06-24 12:56:32 -0700

    first attempt to upload to git python belt exam

--- app/config/routes.py
+++ app/config/routes.py
@@ -16,7 +16,15 @@ from system.core.router import routes
     named id the automatically generated url would be '/products/add/<id>'
     The automatically generated routes respond to all of the http verbs (GET, POST, PUT, PATCH, DELETE)
 """
-routes['default_controller'] = 'Welcome'
+routes['default_controller'] = 'Users'
+routes['/pokes'] = 'Users#pokesdashboard'
+routes['/users/logout'] = 'Users#logout'
+
+
+routes['POST']['/login'] = 'Users#login'
+routes['POST']['/register'] = 'Users#register'
+routes['POST']['/pokeuser/<id>'] = 'Users#pokeuser'
+
 """
     You can add routes and specify their handlers as follows:
 
